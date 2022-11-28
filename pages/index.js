aa30eba7a81a89d3d5d4110a72cd2b5e67177f26
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-28 18:26:49 +0100

    Add external stylesheets with Document

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
 
 export default function Home() {
-  return <h1>Hello word in NextJS!</h1>;
+  return (
+    <>
+   
+      <h1>Hello word in NextJS!</h1>
+    </>
+  );
 }
