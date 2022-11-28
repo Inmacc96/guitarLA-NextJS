4ec31916815fedf86b46a03f73628d85069bf7ca
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-28 20:06:37 +0100

    Define the Footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,22 @@
+import Link from "next/link";
+
+const styles = {};
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <div className="container">
+        <nav className={styles.nav}>
+          <Link href="/">Home</Link>
+          <Link href="/aboutus">About Us</Link>
+          <Link href="/store">Store</Link>
+          <Link href="/blog">Blog</Link>
+        </nav>
 
-export default Footer
+        <p> All rights reserved &copy; {new Date().getFullYear()}</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
