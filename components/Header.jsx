4a88c03a287006b65d188fcf84594f70a89655b2
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-28 19:57:36 +0100

    Add a link in the logo to go to the homepage

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import Nav from "./Nav";
 import styles from "../styles/header.module.css";
 
@@ -6,7 +7,9 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={`container ${styles.headerContent}`}>
-        <Image src="/img/logo.svg" width={300} height={400} alt="logo app" />
+        <Link href={"/"}>
+          <Image src="/img/logo.svg" width={300} height={400} alt="logo app" />
+        </Link>
 
         <Nav />
       </div>
