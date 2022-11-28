4fe75981fb6d4bbe47d66968389b6e6c37cf1634
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-28 19:57:13 +0100

    Highlight the current page

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,13 +1,33 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "../styles/nav.module.css";
 
 const Nav = () => {
+  const router = useRouter();
+
   return (
     <nav className={styles.nav}>
-      <Link href="/">Home</Link>
-      <Link href="/aboutus">About Us</Link>
-      <Link href="/store">Store</Link>
-      <Link href="/blog">Blog</Link>
+      <Link href="/" className={router.pathname === "/" ? styles.active : ""}>
+        Home
+      </Link>
+      <Link
+        href="/aboutus"
+        className={router.pathname === "/aboutus" ? styles.active : ""}
+      >
+        About Us
+      </Link>
+      <Link
+        href="/store"
+        className={router.pathname === "/store" ? styles.active : ""}
+      >
+        Store
+      </Link>
+      <Link
+        href="/blog"
+        className={router.pathname === "/blog" ? styles.active : ""}
+      >
+        Blog
+      </Link>
     </nav>
   );
 };
