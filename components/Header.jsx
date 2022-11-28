f95164ae47d3984286f882e85cd227e9df8e3965
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-28 19:50:38 +0100

    Add styles to header and navigation

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Nav from "./Nav";
+import styles from "../styles/header.module.css";
 
 const Header = () => {
   return (
-    <header>
-      <div className="container">
+    <header className={styles.header}>
+      <div className={`container ${styles.headerContent}`}>
         <Image src="/img/logo.svg" width={300} height={400} alt="logo app" />
 
         <Nav />
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
+import styles from "../styles/nav.module.css";
 
 const Nav = () => {
   return (
-    <nav>
+    <nav className={styles.nav}>
       <Link href="/">Home</Link>
       <Link href="/aboutus">About Us</Link>
       <Link href="/store">Store</Link>
