bb8834374d01d07e40e77862dbd2f6b4548810b1
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-29 11:07:18 +0100

    Get data in dynamic routing with getServerSideProps

--- pages/guitars/[url].jsx
+++ pages/guitars/[url].jsx
@@ -1,5 +1,17 @@
-const Guitar = () => {
+const Guitar = ({ guitar }) => {
+  console.log(guitar);
   return <div>Guitar</div>;
 };
 
 export default Guitar;
+
+export async function getServerSideProps({ query: { url } }) {
+  const response = await fetch(
+    `${process.env.API_URL}/guitars?filters[url]=${url}&populate=image`
+  );
+  const { data: guitar } = await response.json();
+
+  return {
+    props: { guitar },
+  };
+}
