057a0a7f4ba02c0ab8b4df44b79810ce74309063
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-29 11:18:16 +0100

    Get data in dynamic routing with getStaticPaths and getStaticProps

--- pages/guitars/[url].jsx
+++ pages/guitars/[url].jsx
@@ -5,7 +5,23 @@ const Guitar = ({ guitar }) => {
 
 export default Guitar;
 
-export async function getServerSideProps({ query: { url } }) {
+export async function getStaticPaths() {
+  const response = await fetch(`${process.env.API_URL}/guitars`);
+  const { data } = await response.json();
+
+  const paths = data.map((guitar) => ({
+    params: {
+      url: guitar.attributes.url,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { url } }) {
   const response = await fetch(
     `${process.env.API_URL}/guitars?filters[url]=${url}&populate=image`
   );
@@ -15,3 +31,15 @@ export async function getServerSideProps({ query: { url } }) {
     props: { guitar },
   };
 }
+
+/* export async function getServerSideProps({ query: { url } }) {
+    const response = await fetch(
+      `${process.env.API_URL}/guitars?filters[url]=${url}&populate=image`
+    );
+    const { data: guitar } = await response.json();
+  
+    return {
+      props: { guitar },
+    };
+  }
+   */
