a5c8e08db49a8e002375d080747397ca6e537763
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-30 16:13:07 +0100

    Display the contents of the cart

--- pages/cart.js
+++ pages/cart.js
@@ -1,7 +1,8 @@
+import Image from "next/image";
 import Layout from "../components/Layout";
 import styles from "../styles/cart.module.css";
 
-const Cart = () => {
+const Cart = ({ cart }) => {
   return (
     <Layout title="Shopping Cart">
       <main className="container">
@@ -10,6 +11,31 @@ const Cart = () => {
         <div className={styles.content}>
           <div className={styles.cart}>
             <h2>Articles</h2>
+
+            {cart.length === 0
+              ? "Empty cart"
+              : cart.map((product) => (
+                  <div key={product} className={styles.product}>
+                    <div>
+                      <Image
+                        width={250}
+                        height={480}
+                        src={product.image}
+                        alt={`${product.name} guitar`}
+                      />
+                    </div>
+                    <div>
+                      <p className={styles.name}>{product.name}</p>
+                      <p className={styles.price}>
+                        $<span>{product.price}</span>
+                      </p>
+                      <p className={styles.subtotal}>
+                        Subtotal: $
+                        <span>{product.price * product.quantity}</span>
+                      </p>
+                    </div>
+                  </div>
+                ))}
           </div>
 
           <aside className={styles.summary}>
